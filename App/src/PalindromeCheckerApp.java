0f4b6cac036030ce567782aa34dbdb4cb3d15af2
mk2634-create <mk2634@srmist.edu.in>
2026-02-25 08:52:08 +0530

    Muskan Added UC1 Application Entry

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,14 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("       Palindrome Checker App");
+        System.out.println("=================================");
+
+        System.out.println("Version: 1.0");
+        System.out.println("UC1: Application Entry & Welcome Message");
+
+        System.out.println("\nApplication Started Successfully!");
+    }
+}
